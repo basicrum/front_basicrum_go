2da0fceab005b70a24d2d73792a5d355e0768573
mar1n3r0 <m.kirkov@gmail.com>
2023-10-17 15:22:20 +0300

    WIP TestService_processEvent

--- service/service_test.go
+++ service/service_test.go
@@ -3,10 +3,14 @@ package service
 import (
 	"testing"
 
-	"github.com/basicrum/front_basicrum_go/backup"
+	backupmocks "github.com/basicrum/front_basicrum_go/backup/mocks"
+	"github.com/basicrum/front_basicrum_go/beacon"
 	"github.com/basicrum/front_basicrum_go/dao"
 	"github.com/basicrum/front_basicrum_go/geoip"
 	"github.com/basicrum/front_basicrum_go/types"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/ua-parser/uap-go/uaparser"
 )
 
@@ -16,32 +20,113 @@ func TestService_processEvent(t *testing.T) {
 		userAgentParser     *uaparser.Parser
 		events              chan *types.Event
 		geoIPService        geoip.Service
-		hosts               map[string]string
 		subscriptionService ISubscriptionService
-		backupService       backup.IBackup
 	}
 	type args struct {
-		event *types.Event
+		rumEvent beacon.RumEvent
+		lookup   Lookup
+	}
+	type expects struct {
+		GetSubscription  bool
+		processRumEvent  bool
+		rumEvent         beacon.RumEvent
+		SaveExpired      bool
+		SaveExpiredEvent *types.Event
+		SaveUnknown      bool
+		SaveUnknownEvent *types.Event
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name    string
+		fields  fields
+		args    args
+		expects expects
+		want    string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Lookup subscription",
+			args: args{
+				rumEvent: beacon.RumEvent{},
+			},
+			expects: expects{
+				GetSubscription: true,
+				rumEvent:        beacon.RumEvent{},
+			},
+			want: string(FoundLookup),
+		},
+		{
+			name: "Lookup subscription found",
+			args: args{
+				lookup: FoundLookup,
+			},
+			expects: expects{
+				processRumEvent: true,
+				rumEvent:        beacon.RumEvent{},
+			},
+			want: string(ExpiredLookup),
+		},
+		{
+			name: "Lookup subscription expired",
+			args: args{
+				lookup: FoundLookup,
+			},
+			expects: expects{
+				SaveExpired:      true,
+				SaveExpiredEvent: &types.Event{},
+			},
+			want: string(ExpiredLookup),
+		},
+		{
+			name: "Lookup subscription not found",
+			args: args{
+				lookup: ExpiredLookup,
+			},
+			expects: expects{
+				SaveUnknown:      true,
+				SaveUnknownEvent: &types.Event{},
+			},
+			want: string(NotFoundLookup),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &Service{
-				daoService:          tt.fields.daoService,
-				userAgentParser:     tt.fields.userAgentParser,
-				events:              tt.fields.events,
-				geoIPService:        tt.fields.geoIPService,
-				hosts:               tt.fields.hosts,
-				subscriptionService: tt.fields.subscriptionService,
-				backupService:       tt.fields.backupService,
+			// s := &Service{
+			// 	daoService:          tt.fields.daoService,
+			// 	userAgentParser:     tt.fields.userAgentParser,
+			// 	events:              tt.fields.events,
+			// 	geoIPService:        tt.fields.geoIPService,
+			// 	hosts:               tt.fields.hosts,
+			// 	subscriptionService: tt.fields.subscriptionService,
+			// 	backupService:       tt.fields.backupService,
+			// }
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			backupService := backupmocks.NewMockIBackup(ctrl)
+			s := New(tt.fields.daoService, tt.fields.userAgentParser, tt.fields.geoIPService, tt.fields.subscriptionService, backupService)
+			for e := range tt.fields.events {
+				b := beacon.FromEvent(e)
+				re := beacon.ConvertToRumEvent(b, e, tt.fields.userAgentParser, tt.fields.geoIPService)
+				lk, err := s.subscriptionService.GetSubscription(re.SubscriptionID, re.Hostname)
+				require.NoError(t, err)
+				assert.Equal(t, tt.want, lk)
+				if tt.expects.processRumEvent {
+					s.processRumEvent.EXPECT().
+						processRumEvent(
+							tt.expects.rumEvent,
+						)
+				}
+				if tt.expects.SaveExpired {
+					backupService.EXPECT().
+						SaveExpired(
+							tt.expects.SaveExpiredEvent,
+						)
+				}
+				if tt.expects.SaveUnknown {
+					backupService.EXPECT().
+						SaveUnknown(
+							tt.expects.SaveUnknownEvent,
+						)
+				}
 			}
-			s.processEvent(tt.args.event)
 		})
 	}
 }
